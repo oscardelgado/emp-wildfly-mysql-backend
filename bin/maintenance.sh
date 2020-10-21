3e4f69c6f4367eba9dba0cfc6feba69dcd24b9cb
Óscar Delgado Santamaría <oscardelgado83@gmail.com>
2020-10-21 21:51:37 +0200

    Update maintenance.sh

--- bin/maintenance.sh
+++ bin/maintenance.sh
@@ -2,6 +2,6 @@
 echo "Starting maintenance..."
 mysql -u $BD_USER -p'$BD_PW' \
         -h us-cdbr-iron-east-03.cleardb.net -P 3306 \
-        -D database \
+        -D heroku_a304a837b2c7d20 \
         --execute="select count(*) from heroku_a304a837b2c7d20.ExportPOJO;"
 echo "Done maintenance"
