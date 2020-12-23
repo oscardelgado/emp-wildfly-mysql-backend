823830a1c6dcfd3c11f580ba748dbf43f9df278d
Óscar Delgado Santamaría <oscardelgado83@gmail.com>
2020-12-23 08:39:57 +0100

    Change threshold to 3 weeks

--- bin/maintenance.sh
+++ bin/maintenance.sh
@@ -3,5 +3,5 @@ echo "Starting maintenance..."
 mysql -u $BD_USER -p$BD_PW \
         -h us-cdbr-iron-east-03.cleardb.net -P 3306 \
         -D heroku_a304a837b2c7d20 \
-        --execute="select count(*) from heroku_a304a837b2c7d20.ExportPOJO;delete from heroku_a304a837b2c7d20.ExportPOJO where updateTimestamp < DATE_SUB(curdate(), INTERVAL 1 MONTH) and accountName <> 'test';select count(*) from heroku_a304a837b2c7d20.ExportPOJO;optimize table heroku_a304a837b2c7d20.exportpojo;"
+        --execute="select count(*) from heroku_a304a837b2c7d20.ExportPOJO;delete from heroku_a304a837b2c7d20.ExportPOJO where updateTimestamp < DATE_SUB(curdate(), INTERVAL 21 DAY) and accountName <> 'test';select count(*) from heroku_a304a837b2c7d20.ExportPOJO;optimize table heroku_a304a837b2c7d20.exportpojo;"
 echo "Done maintenance"
