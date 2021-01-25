aba573820bbd5bc479e1d89b8cc8e85c17258f45
Óscar Delgado Santamaría <oscardelgado83@gmail.com>
2021-01-25 09:25:42 +0100

    Update ClearDB data

--- bin/maintenance.sh
+++ bin/maintenance.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Starting maintenance..."
 mysql -u $BD_USER -p$BD_PW \
-        -h us-cdbr-iron-east-03.cleardb.net -P 3306 \
-        -D heroku_a304a837b2c7d20 \
-        --execute="select count(*) from heroku_a304a837b2c7d20.ExportPOJO;delete from heroku_a304a837b2c7d20.ExportPOJO where updateTimestamp < DATE_SUB(curdate(), INTERVAL 21 DAY) and accountName <> 'test';select count(*) from heroku_a304a837b2c7d20.ExportPOJO;optimize table heroku_a304a837b2c7d20.exportpojo;"
+        -h us-cdbr-east-03.cleardb.com -P 3306 \
+        -D heroku_9f31afb0d154a85 \
+        --execute="select count(*) from heroku_9f31afb0d154a85.ExportPOJO;delete from heroku_9f31afb0d154a85.ExportPOJO where updateTimestamp < DATE_SUB(curdate(), INTERVAL 21 DAY) and accountName <> 'test';select count(*) from heroku_a304a837b2c7d20.ExportPOJO;optimize table heroku_a304a837b2c7d20.exportpojo;"
 echo "Done maintenance"
